dc78d3bda58928fd32a442ed342e02aa9984c205
Lithrun <nickwindt@hotmail.nl>
2021-02-20 00:40:12 +0100

    Remove scripts from PUN.Editor

--- index.js
+++ index.js
@@ -17,8 +17,12 @@ try {
 
   files.forEach(file => {
       console.log('Attempting to overwrite: ' + file);
-      
       let content = fs.readFileSync('./' + file, "utf8");
+
+      if (file.includes("PhotonUnityNetworking.Editor.csproj")) {
+        content = content.replace(/<Compile.*\/>/g, "");
+      }
+
       content = content.replace(/\/opt\/unity\/Editor\/Data\/Managed\/UnityEngine\/UnityEditor/g, "/github/workspace/SonarQube/UnityEditor/UnityEditor");
       content = content.replace(/\/opt\/unity\/Editor\/Data\/Managed\/UnityEngine/g, "/github/workspace/Library/PlayerDataCache/Win64/Data/Managed");
       content = content.replace(/\/opt\/unity\/Editor\/Data\/MonoBleedingEdge\/lib\/mono/g, "/github/workspace/SonarQube/mono");
