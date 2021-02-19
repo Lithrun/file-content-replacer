218b8d91e40a1a9d5564173b79472d0bcbaf1b5c
Lithrun <nickwindt@hotmail.nl>
2021-02-19 19:38:59 +0100

    Ignore external projects

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 
+  const ignoredCsproj = ["PhotonUnityNetworking.Editor.csproj", "PhotonUnityNetworking.Utlity.csproj", "PhotonUnityNetworking.csproj", "Discord.csproj"];
   fs.readdirSync('./').forEach(x => {
       console.log(x);
   });
@@ -17,14 +18,19 @@ try {
 
   files.forEach(file => {
       console.log('Attempting to overwrite: ' + file);
-      let content = fs.readFileSync('./' + file, "utf8");
-      content = content.replace(/\/opt\/unity\/Editor\/Data\/Managed\/UnityEngine\/UnityEditor/g, "/github/workspace/SonarQube/UnityEditor/UnityEditor");
-      content = content.replace(/\/opt\/unity\/Editor\/Data\/Managed\/UnityEngine/g, "/github/workspace/Library/PlayerDataCache/Win64/Data/Managed");
-      content = content.replace(/\/opt\/unity\/Editor\/Data\/MonoBleedingEdge\/lib\/mono/g, "/github/workspace/SonarQube/mono");
-      content = content.replace(/\/opt\/unity\/Editor\/Data\/PlaybackEngines/g, "/github/workspace/SonarQube/PlaybackEngines");
-      content = content.replace(/<Csc.*>/g, "");
-      content = content.replace(/\/opt\/unity\/Editor\/Data\/Managed/g, "/github/workspace/SonarQube/UnityEditor");
-      fs.writeFileSync('./' + file, content);
+      
+      if (ignoredCsproj.includes(file)) {
+        fs.unlinkSync(file);
+      } else {
+        let content = fs.readFileSync('./' + file, "utf8");
+        content = content.replace(/\/opt\/unity\/Editor\/Data\/Managed\/UnityEngine\/UnityEditor/g, "/github/workspace/SonarQube/UnityEditor/UnityEditor");
+        content = content.replace(/\/opt\/unity\/Editor\/Data\/Managed\/UnityEngine/g, "/github/workspace/Library/PlayerDataCache/Win64/Data/Managed");
+        content = content.replace(/\/opt\/unity\/Editor\/Data\/MonoBleedingEdge\/lib\/mono/g, "/github/workspace/SonarQube/mono");
+        content = content.replace(/\/opt\/unity\/Editor\/Data\/PlaybackEngines/g, "/github/workspace/SonarQube/PlaybackEngines");
+        content = content.replace(/<Csc.*>/g, "");
+        content = content.replace(/\/opt\/unity\/Editor\/Data\/Managed/g, "/github/workspace/SonarQube/UnityEditor");
+        fs.writeFileSync('./' + file, content);
+      }
   });
 } catch (error) {
   core.setFailed(error.message);
